7cfdad3ff7247f588690b940a56c15c69d7f17ff
Andrew Miller <andrew@dragonchain.com>
2024-02-06 13:21:01 -0500

    Updated deployment script

--- deploy/local/00_coiin.js
+++ deploy/local/00_coiin.js
@@ -1,13 +1,23 @@
 const { ethers, upgrades } = require("hardhat");
 
 async function main() {
+    const [deployer] = await ethers.getSigners();
+
     const Coiin = await ethers.getContractFactory("Coiin");
     const coiin = await upgrades.deployProxy(
         Coiin,
-        ["CoiinAM", "COIINAM"],
+        [
+            deployer.address,
+            "0x4B7DC4697d860efFF0F1CF073D466b9e68c7cb47",
+            deployer.address,
+            "Coiin",
+            "COIIN"
+        ],
         { initializer: 'initialize' }
     );
-    await coiin.deployed();
+    await coiin.waitForDeployment();
+
+    console.log("Contract Address:", coiin.target);
 }
 
 main();
