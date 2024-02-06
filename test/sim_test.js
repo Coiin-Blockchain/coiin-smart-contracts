42b90108d50dc6169386c2d56b6d8026420bdea5
yielddev <80984045+yielddev@users.noreply.github.com>
2024-02-06 12:15:36 -0700

    sim_test

--- test/sim_test.js
+++ test/sim_test.js
@@ -22,6 +22,57 @@ const getRandomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const verifySignature = async function (sender, amount, expires, nonce, coiin, signature) {
+    const [ owner, otherAccount, signer, multiSig, mockUser1, mockUser2, mockUser3 ] = await ethers.getSigners();
+    let message = ethers.solidityPackedKeccak256(
+        ["address", "uint256", "uint256", "uint256", "address"],
+        [sender, amount, expires, nonce, coiin]
+    )
+
+    let messageHash = ethers.hashMessage(ethers.getBytes(message))
+    let verification = await ethers.verifyMessage(ethers.getBytes(message), signature)
+    console.log(verification)
+    return verification
+
+}
+describe("sig check", function () {
+    it("check sig", async function() {
+        let coiin = await ethers.getContractFactory("Coiin");
+        console.log("check sig")
+        let verify = await verifySignature(
+            "0x2403A8C8A132ACf151AAb0f26b355b370aA18e5a", 
+            1,
+            1707189696,
+            '8470809963594808849',
+            "0x1E513C77Fd27702297a102Bc6E4adE062B8481f2",
+            "0xf8c8920728f0940ef86b15286e7a8a03d400dd4960167b3b5528b90c37e659063dca877b5436649915cd1c945d4edc1b5070b5a5a2b13942db0522239d080c3d00"
+        )
+        console.log(verify)
+        console.log("Error Sigs") 
+        let interface = coiin.interface
+        let expired = interface.getError("Coiin__Expired")
+        let Coiin__InvalidSignature = interface.getError("Coiin__InvalidSignature")
+        let Coiin__MaxWithdrawAccountLimit = interface.getError("Coiin__MaxWithdrawAccountLimit")
+        let Coiin__MaxWithdrawClusterLimit = interface.getError("Coiin__MaxWithdrawClusterLimit")
+        let Coiin__MaxWithdrawLimit = interface.getError("Coiin__MaxWithdrawLimit")
+        let Coiin__BalanceTooLow = interface.getError("Coiin__BalanceTooLow")
+        let Coiin__InvalidNonce = interface.getError("Coiin__InvalidNonce")
+        let Coiin__ZeroAmount = interface.getError("Coiin__ZeroAmount")
+        console.log("====Errors======")
+        console.log("Expired: ", expired.selector)
+        console.log("InvalidSignature: ", Coiin__InvalidSignature.selector)
+        console.log("MaxWithdrawAccountLimit: ", Coiin__MaxWithdrawAccountLimit.selector)
+        console.log("MaxWithdrawClusterLimit: ", Coiin__MaxWithdrawClusterLimit.selector)
+        console.log("MaxWithdrawLimit: ", Coiin__MaxWithdrawLimit.selector)
+        console.log("BalanceTooLow: ", Coiin__BalanceTooLow.selector)
+        console.log("InvalidNonce: ", Coiin__InvalidNonce.selector)
+        console.log("ZeroAmount: ", Coiin__ZeroAmount.selector)
+
+
+
+    })
+})
+
 describe("Simulation Testing", function () {
     async function deployCoiinFixture() {
         // Contracts are deployed using the first signer/account by default
